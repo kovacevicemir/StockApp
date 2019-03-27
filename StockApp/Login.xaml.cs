1df1be9cddd9ea80eae7d4fe8bb8a4fd75bbac6b
Emir Kovacevic <emirkovacevic92@gmail.com>
2019-03-28 01:27:12 +1000

    first commit

---
+++ StockApp/Login.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace StockApp
+{
+    /// <summary>
+    /// Interaction logic for Login.xaml
+    /// </summary>
+    public partial class Login : Window
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private async void Login_btn_Click(object sender, RoutedEventArgs e)
+        {
+            loader.Visibility = Visibility.Visible;
+            await Task.Run(() => Thread.Sleep(3000));
+            loader.Visibility = Visibility.Collapsed;
+        }
+    }
+}
