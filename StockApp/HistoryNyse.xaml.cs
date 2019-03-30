1492185f4f3cd2a1a209e3813e4ca9a77cf177d2
Emir Kovacevic <emirkovacevic92@gmail.com>
2019-03-31 02:13:21 +1000

    added: History-> ComboBox, Search model, Inputs... DateFormatFail

--- StockApp/HistoryNyse.xaml.cs
+++ StockApp/HistoryNyse.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using StockApp.Models;
 
 namespace StockApp
 {
@@ -55,6 +56,14 @@ namespace StockApp
             //Insert data into datagrid_products
             dataGrid_nyse.ItemsSource = dt1.DefaultView;
             dataGrid_nyse.CanUserAddRows = false;
+
+            //Fill combo box exchange
+            string exchange = "exchange";
+            fill_combo_box(exchangeInput, exchange);
+
+            //Fill combo box symbol
+            string symbol = "stock_symbol";
+            fill_combo_box(symbolInput, symbol);
         }
 
         //FETCH All FROM DB / PUT IT IN DataTable x
@@ -69,6 +78,122 @@ namespace StockApp
             SqlDataAdapter da1 = new SqlDataAdapter(cmd2);
             da1.Fill(dt1);
             return dt1;
+        }
+
+        //SEARCH HANDLER
+        private void SearchBtn_Click(object sender, RoutedEventArgs e)
+        {
+            //Store search inputs into Search (Nsye model)
+            Nsye Search = new Nsye();
+
+            //Search.exchange = exchangeInput.SelectedValue.ToString();
+            //Search.stock_symbol = symbolInput.SelectedValue.ToString();
+
+            //Open
+            if(openFromInput.Text != "")
+            {
+                Search.stock_price_open_from = float.Parse(openFromInput.Text);
+            }
+
+            if (openToInput.Text != "")
+            {
+                Search.stock_price_open_to = float.Parse(openToInput.Text);
+            }
+
+            //Close
+            if (closeInputFrom.Text != "")
+            {
+                Search.stock_price_close_from = float.Parse(closeInputFrom.Text);
+            }
+
+            if (closeInputTo.Text != "")
+            {
+                Search.stock_price_close_to = float.Parse(closeInputTo.Text);
+            }
+
+            //High
+            if (highInputFrom.Text != "")
+            {
+                Search.stock_price_high_from = float.Parse(highInputFrom.Text);
+            }
+
+            if (highInputTo.Text != "")
+            {
+                Search.stock_price_high_to = float.Parse(highInputTo.Text);
+            }
+
+            //Low
+            if (lowInputFrom.Text != "")
+            {
+                Search.stock_price_low_from = float.Parse(lowInputFrom.Text);
+            }
+
+            if (lowInputTo.Text != "")
+            {
+                Search.stock_price_low_to = float.Parse(lowInputTo.Text);
+            }
+
+            //Volume
+            if (volumeInputFrom.Text != "")
+            {
+                Search.stock_volume_from = int.Parse(volumeInputFrom.Text);
+            }
+
+            if (volumeInputTo.Text != "")
+            {
+                Search.stock_volume_to = int.Parse(volumeInputTo.Text);
+            }
+
+            //Adj
+            if (adjInputFrom.Text != "")
+            {
+                Search.stock_price_adj_close_from = float.Parse(adjInputFrom.Text);
+            }
+
+            if (adjInputTo.Text != "")
+            {
+                Search.stock_price_adj_close_to = float.Parse(adjInputTo.Text);
+            }
+
+            //Date
+            Search.date_from = dateFrom.SelectedDate.Value.ToString("d/MM/yyyy");
+            MessageBox.Show(Search.date_from);
+            Search.date_to = dateTo.SelectedDate.Value.ToString("d/MM/yyyy");
+            MessageBox.Show(Search.date_to);
+
+            //Return Search
+            string q = "select * from nyse_history WHERE date BETWEEN " +"'"+Search.date_from+"'" +" AND " + "'"+Search.date_to+"'";
+            string q1 = "select * from nyse_history where date = '7/03/2005'";
+            //SELECT* FROM nyse_history WHERE date BETWEEN '3/03/2005' and '3/03/2006';
+            DataTable dt2 = FetchProducts(q);
+
+            //Insert data into datagrid_products
+            dataGrid_nyse.ItemsSource = dt2.DefaultView;
+            dataGrid_nyse.CanUserAddRows = false;
+
+        }
+
+        //COMBOBOX
+        public void fill_combo_box(ComboBox comboBox, string header)
+        {
+            comboBox.Items.Clear();
+
+            //Select all from units table
+            SqlCommand cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "SELECT DISTINCT " +header+ " FROM nyse_history";
+            cmd.ExecuteNonQuery();
+
+            //create new datatable and fill with cmd
+            DataTable dt = new DataTable();
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            da.Fill(dt);
+
+            //iterate dt and add each row ["unit"] to combo box item
+            foreach (DataRow row in dt.Rows)
+            {
+                comboBox.Items.Add(row[header].ToString());
+            }
         }
     }
 }
--- StockApp/Models/Nsye.cs
+++ StockApp/Models/Nsye.cs
@@ -10,12 +10,27 @@ namespace StockApp.Models
     {
         public string exchange { get; set; }
         public string stock_symbol { get; set; }
-        public DateTime date { get; set; }
-        public float stock_price_open { get; set; }
-        public float stock_price_high { get; set; }
-        public float stock_price_low { get; set; }
-        public float stock_price_close { get; set; }
-        public int stock_volume { get; set; }
-        public float stock_price_adj_close { get; set; }
+
+        public string date_from { get; set; }
+        public string date_to { get; set; }
+
+        public float stock_price_open_from { get; set; } = 1;
+        public float stock_price_open_to { get; set; } = 1000;
+
+        public float stock_price_high_from { get; set; } = 1;
+        public float stock_price_high_to { get; set; } = 1000;
+
+        public float stock_price_low_from { get; set; } = 1;
+        public float stock_price_low_to { get; set; } = 1000;
+
+        public float stock_price_close_from { get; set; } = 1;
+        public float stock_price_close_to { get; set; } = 1000;
+
+        public int stock_volume_from { get; set; } = 1;
+        public int stock_volume_to { get; set; } = 999999999;
+
+        public float stock_price_adj_close_from { get; set; } = 1;
+        public float stock_price_adj_close_to { get; set; } = 1000;
+
     }
 }
